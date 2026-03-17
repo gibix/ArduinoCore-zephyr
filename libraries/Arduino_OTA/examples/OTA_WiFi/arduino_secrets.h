c79f21050df396e98f0f7f224a9065b898c03a70
Gilberto Conti <g.conti@arduino.cc>
2026-03-17 17:16:49 +0100

    fix licence
    Signed-off-by: Gilberto Conti <g.conti@arduino.cc>

--- libraries/Arduino_OTA/examples/OTA_WiFi/arduino_secrets.h
+++ libraries/Arduino_OTA/examples/OTA_WiFi/arduino_secrets.h
@@ -1,2 +1,8 @@
+/*
+ * Copyright (c) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
 #define SECRET_SSID "your-ssid"
 #define SECRET_PASS "your-password"
